664dac1052e55c485d2f6d43b62a7f439823f632
CHIH JOU HUANG <chihjouhuang@CHIHdeMacBook-Pro.local>
2023-07-06 09:50:28 +0800

    first commit

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,12 @@
+class HomeController < ApplicationController
+
+    def index
+    end
+
+    def first
+    end
+
+    def second
+    end
+
+end
---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+  root "home#index"
+
+  get "first", to: "home#first"
+  get "second", to: "home#second"
+end
